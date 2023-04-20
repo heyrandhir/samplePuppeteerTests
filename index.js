4d4cd114e2e0dcf8335e6682f78f914e23d95d37
Randhir Kumar Singh <97341921+heyrandhir@users.noreply.github.com>
2023-04-20 06:18:14 +0530

    added index html and js file

---
+++ index.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector('#btn');
+const counter = document.querySelector('#counter');
+btn.addEventListener('click', () => {
+  counter.innerHTML = +counter.textContent + 1;
+});
