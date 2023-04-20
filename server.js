032515bff245a31ed212615faca0b94afe309743
Randhir Kumar Singh <97341921+heyrandhir@users.noreply.github.com>
2023-04-20 07:52:50 +0530

    modify server js to listen for js file

--- server.js
+++ server.js
@@ -4,13 +4,36 @@ const port = process.env.PORT || 8000;
 
 http.createServer(function (req, res) {
     console.log(`Request received for '${req.url}'`);
-    fs.readFile(__dirname + "/index.html", function (err, data) {
-        if (err) {
-            res.writeHead(404);
-            res.end(JSON.stringify(err));
-            return;
-        }
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-    })
+
+    // Serve the index.html file
+    if (req.url === "/" || req.url === "/index.html") {
+        fs.readFile(__dirname + "/index.html", function (err, data) {
+            if (err) {
+                res.writeHead(404);
+                res.end(JSON.stringify(err));
+                return;
+            }
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(data);
+        });
+    }
+
+    // Serve the JavaScript file
+    else if (req.url === "/index.js") {
+        fs.readFile(__dirname + "/index.js", function (err, data) {
+            if (err) {
+                res.writeHead(404);
+                res.end(JSON.stringify(err));
+                return;
+            }
+            res.writeHead(200, { "Content-Type": "text/javascript" });
+            res.end(data);
+        });
+    }
+
+    // Serve any other file as 404
+    else {
+        res.writeHead(404);
+        res.end("File not found");
+    }
 }).listen(port, () => console.log(`Listening on port ${port}`));
